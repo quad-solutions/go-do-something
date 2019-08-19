970c4995d11c760180a65271f3c48d89eec6abc0
G710 <16511117+G710@users.noreply.github.com>
2019-08-19 21:35:02 +0200

    Updates

---
+++ precache-manifest.5534e917f5d591b3d3ad1e215d7653bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fed11d905027f53ee38c201dc41e5a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fa1a2bc4b074ced4b3",
+    "url": "/static/css/main.d025d18f.chunk.css"
+  },
+  {
+    "revision": "0fcf1d432577baeb814d",
+    "url": "/static/js/2.5e61679c.chunk.js"
+  },
+  {
+    "revision": "e2fa1a2bc4b074ced4b3",
+    "url": "/static/js/main.ee877cea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
